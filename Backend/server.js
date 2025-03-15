b20e8f0aa0d1c78c0839ef53bccdc711202552d3
Hiruni <kathaluwagehiruni@gmail.com>
2025-03-15 23:44:19 +0530

    Fixed db connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,47 @@
-import express from 'express';
-import dotenv from 'dotenv';
 
+const express = require('express');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+const cors = require('cors');
+//const multer = require('multer')
+//const path =require('path')
+//const vision = require('@google-cloud/vision');
+const app = express();
+const prescriptionRoutes = require("./routes/prescriptionRoutes");
 dotenv.config();
 
-const app = express();
 
-app.get("/products", (req, res) => {
+app.use(cors());
+app.use(express.json());
+app.use("/prescription", prescriptionRoutes);
+//app.use("/files",express.static("files"));
 
-    
-    res.send("server is ready");
-});
 
-console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error("MongoDB Connection Error:", err));
+
+
+
+/*const storage = multer.diskStorage({
+    destination: function (req,file, cb) {
+
+        const uniqueSuffix = Date.now()
+        cb(null, uniqueSuffix + file.originalname);
+    },
+
+})
+
+const Upload = multer({
+    storage:storage
+}) */
+
 
 app.listen(5080, () => {
     console.log("Server started at http://localhost:5080 Hello");
 });
-
