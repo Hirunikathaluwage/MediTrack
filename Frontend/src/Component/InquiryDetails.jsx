a3df5c9e427ae1d232ee6cacfd53a06673e0b457
Awantha <awanthaimesh65@gmail.com>
2025-03-15 20:01:46 +0530

    ✅ Successfully built the Display & Delete

---
+++ Frontend/src/Component/InquiryDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InquiryDetails = () => {
+    return (
+        <div>
+            <h1>Inquiry Details</h1>
+            {/* Add your component code here */}
+        </div>
+    );
+};
+
+export default InquiryDetails;
