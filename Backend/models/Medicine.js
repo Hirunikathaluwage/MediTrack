e6bb1b0bf1399773a521f794c6389f6acd4cfaf1
Hiruni <kathaluwagehiruni@gmail.com>
2025-03-17 20:31:39 +0530

    Removed .env file

---
+++ Backend/models/Medicine.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+const medicineSchema = new mongoose.Schema({
+    medicineId: {
+        type: Number,
+        required : true,
+    },
+
+    name: {
+        type : String,
+        required : true,
+    },
+
+    genericName: {
+        type : String,
+        required : true,
+    },
+
+    price: {
+        type : Number,
+        required : true,
+    },
+
+    unit: {
+        type : String,
+        required : true,
+    },
+
+    description: {
+        type : String,
+        required : true,
+    },
+
+    expireDate: {
+        type : Date,
+        required : true,
+    },
+
+    manufactureDate: {
+        type : Date,
+        required : true,
+    },
+});
+
+//module.exports = mongoose.model('Medicine' , medicineSchema, 'medicine ');
+
+export const Medicine = mongoose.model("Medicine", medicineSchema);
+
+export default Medicine;
