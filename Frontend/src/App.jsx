005d5ca7533cc29fed1d577a6ef3004a459c20a0
Hiruni <kathaluwagehiruni@gmail.com>
2025-03-21 01:29:28 +0530

    fixed routes with react dom

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-//import axios from 'axios'
-import './index.css'
-import Prescription from './Prescription'
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './index.css';
+import Prescription from './Prescription';
+import Approval from './Approval';
+import MedicineList from './MedicineList';
 
 function App() {
-  
   return (
-    <Prescription/>
-  )
-
+    <Router>  
+      <Routes>
+        <Route path="/" element={<Prescription />} />
+        <Route path="/MedicineList" element={<MedicineList />} />
+        <Route path="/Approval" element={<Approval />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
