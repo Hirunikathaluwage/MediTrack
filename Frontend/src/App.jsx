3a921b23bdaf30266ced4a14a568e99a7dc8df42
Hiruni <kathaluwagehiruni@gmail.com>
2025-03-23 12:59:57 +0530

    Added reportRouter file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
-import Prescription from './Prescription';
-import Approval from './Approval';
-import MedicineList from './MedicineList';
+import Prescription from './pages/Prescription'
+import MedicineList from './pages/MedicineList'
+import Approval from './component/Approval';
+/*import Reportgen from './pages/Reportgen'
+import Stat from "./component/Stat";
+import TopMedicine from "./component/TopMedicine";
+import BranchOrder from "./component/BranchOrder";
+import Profit from "./component/Profit"; */
+
 
 function App() {
   return (
     <Router>  
       <Routes>
         <Route path="/" element={<Prescription />} />
-        <Route path="/MedicineList" element={<MedicineList />} />
         <Route path="/Approval" element={<Approval />} />
+        <Route path="/MedicineList" element={<MedicineList />} />
+       
+        {/*<Route path="/" element={<Reportgen />} />
+        <Route path="/" element={<Stat />} />
+        <Route path="/" element={<TopMedicine />} />
+        <Route path="/" element={<BranchOrder />} />
+        <Route path="/" element={<Profit />} />*/}
+
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
