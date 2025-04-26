296db53a45aab6fc53fc19fffd79d985485c42d2
osanda02 <osandakalendra@gmail.com>
2025-04-26 17:37:19 +0530

    commented conflict files

--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors'; // Import CORS
-import { connectDB } from './dbconnect.js';
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import cors from 'cors'; // Import CORS
+// import { connectDB } from './dbconnect.js';
 
 import medicineroute from './routes/MedicineRoute.js';
 import branchroute from './routes/BranchRoute.js';
 import branchstockroute from './routes/BranchStockRoute.js';
 import PrescriptionRoute from './routes/PrescriptionRoute.js';
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
-// Enable CORS for all requests
-app.use(cors());
+// // Enable CORS for all requests
+// app.use(cors());
 
-app.use(express.json());
+// app.use(express.json());
 
 app.use("/api/medicines", medicineroute);
 app.use("/api/branch", branchroute);
 app.use("/api/branchstock", branchstockroute);
 app.use("/api/prescription", PrescriptionRoute);
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
+// app.get("/", (req, res) => {
+//     res.send("API is running...");
+// });
 
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 
-app.listen(5080, () => {
-    connectDB();
-    console.log("MediTrack Server started at http://localhost:5080");
-});
+// app.listen(5080, () => {
+//     connectDB();
+//     console.log("MediTrack Server started at http://localhost:5080");
+// });
