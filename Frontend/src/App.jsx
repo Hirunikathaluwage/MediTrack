1640db7738e7fa030fbcff86b55eb4ef5c8cd2de
Hiruni <kathaluwagehiruni@gmail.com>
2025-04-27 16:35:05 +0530

    modified app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,14 @@
 
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import './index.css';
-// import Prescription from './pages/Prescription'
-// // import MedicineList from './pages/ApprovedMedicines'
+// import Prescription from './pages/Prescription.jsx'
+// import MedicineList from './pages/ApprovedMedicines'
 // import Approval from './component/Approval';
-// //import Reportgen from './pages/Reportgen'
-// //import AdminPrescription from './pages/AdminPrescription'
-// import ApprovedMedicines from './pages/ApprovedMedicines'; // was MedicineList
+// import ApprovedMedicines from './pages/ApprovedMedicines'; 
 
-// // import SplitScreenDashboard from "./pages/SplitScreenDashboard.jsx"; deleted this file
-// // import AdminPrescription from "./component/AdminPrescription.jsx";
-// // import  SearchMedicineInBranch from "./component/SearchMedicineInBranch.jsx";
+// import AdminPrescription from "./component/AdminPrescription.jsx";
+// import  SearchMedicineInBranch from "./component/SearchMedicineInBranch.jsx";
 // import SplitPage from "./pages/split.jsx";
-
-// //import Stat from "./component/Stat";
-// //import TopMedicine from "./component/TopMedicine";
-// //import BranchOrder from "./component/BranchOrder";
-// //import Profit from "./component/Profit"; 
 
 
 // function App() {
@@ -24,14 +16,13 @@
 //     <Router>  
 //       <Routes>
 
-
 //         <Route path="/" element={<Prescription />} />
 //         <Route path="/approval" element={<Approval />} />
 //         <Route path="/approved-medicines" element={<ApprovedMedicines />} />
 //         {/* <Route path="/Approval" element={<Approval />} /> */}
-//         {/* <Route path="/MedicineList" element={<MedicineList />} /> */}
+//         <Route path="/MedicineList" element={<MedicineList />} />
 //         {/* <Route path="/reportgen" element={<Reportgen />} /> */}
-//         {/* <Route path="/adminPrescription" element={<AdminPrescription />} /> */}
+//         <Route path="/adminPrescription" element={<AdminPrescription />} />
 //         <Route path="/SearchMedicineInBranch" element={<SearchMedicineInBranch />} />
 //         {/* <Route path="/SplitScreenDashboard" element={<SplitScreenDashboard />} /> not used */}
 //         <Route path="/SplitPage" element={<SplitPage />} />
@@ -48,55 +39,3 @@
 // }
 
 // export default App;
-
-
-
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './index.css';
-import Prescription from './pages/Prescription.jsx'
-import MedicineList from './pages/ApprovedMedicines'
-import Approval from './component/Approval';
-// import Reportgen from './pages/Reportgen'
-//import AdminPrescription from './pages/AdminPrescription'
-import ApprovedMedicines from './pages/ApprovedMedicines'; // was MedicineList
-//import SearchMedicineInBranch from "./pages/SearchMedicineInBranch.jsx";
-// import SplitScreenDashboard from "./pages/SplitScreenDashboard.jsx"; deleted this file
-import AdminPrescription from "./component/AdminPrescription.jsx";
-import  SearchMedicineInBranch from "./component/SearchMedicineInBranch.jsx";
-import SplitPage from "./pages/split.jsx";
-
-//import Stat from "./component/Stat";
-//import TopMedicine from "./component/TopMedicine";
-//import BranchOrder from "./component/BranchOrder";
-//import Profit from "./component/Profit"; 
-
-
-function App() {
-  return (
-    <Router>  
-      <Routes>
-
-        <Route path="/" element={<Prescription />} />
-        <Route path="/approval" element={<Approval />} />
-        <Route path="/approved-medicines" element={<ApprovedMedicines />} />
-        {/* <Route path="/Approval" element={<Approval />} /> */}
-        <Route path="/MedicineList" element={<MedicineList />} />
-        {/* <Route path="/reportgen" element={<Reportgen />} /> */}
-        <Route path="/adminPrescription" element={<AdminPrescription />} />
-        <Route path="/SearchMedicineInBranch" element={<SearchMedicineInBranch />} />
-        {/* <Route path="/SplitScreenDashboard" element={<SplitScreenDashboard />} /> not used */}
-        <Route path="/SplitPage" element={<SplitPage />} />
-       
-       {/* <Route path="/Reportgen" element={<Reportgen />} />
-        <Route path="/Reportgen" element={<Stat />} />
-        <Route path="/Reportgen" element={<TopMedicine />} />
-        <Route path="/Reportgen" element={<BranchOrder />} />
-        <Route path="/Reportgen" element={<Profit />} /> */}
-
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
