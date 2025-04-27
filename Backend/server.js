be945458302ae104bca52673c50a94a2ab81cb5c
Hiruni <kathaluwagehiruni@gmail.com>
2025-04-27 17:01:12 +0530

    Added modified prescription model to main branch

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
+
 // import express from "express";
 // import mongoose from "mongoose";
 // import dotenv from "dotenv";
 // import cors from "cors";
-// import prescriptionRoutes from "./routes/prescriptionRoutes.js";
 
+// import branchRoutes from "./routes/branchRoutes.js"; 
+// import PrescriptionRoute from './routes/PrescriptionRoute.js';
+// import adminPrescriptionRoutes from './routes/AdminPresRoutes.js';
 
 // dotenv.config();
 
@@ -11,52 +14,21 @@
 
 // app.use(cors());
 // app.use(express.json());
-// app.use("/prescription", prescriptionRoutes);
+
 // app.use("/branches", branchRoutes);
-
-// mongoose.connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err) => console.error("MongoDB Connection Error:", err));
-
-// app.listen(5080, () => {
-//   console.log("Server started at http://localhost:5080");
-// });
-
-
-
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
-
-import branchRoutes from "./routes/branchRoutes.js"; 
-import PrescriptionRoute from './routes/PrescriptionRoute.js';
-import adminPrescriptionRoutes from './routes/AdminPresRoutes.js';
-
-dotenv.config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/branches", branchRoutes);
-app.use("/prescription", PrescriptionRoute);
-app.use('/adminprescription', adminPrescriptionRoutes);
+// app.use("/prescription", PrescriptionRoute);
+// app.use('/adminprescription', adminPrescriptionRoutes);
 
 
 
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB Connection Error:", err));
+// // mongoose.connect(process.env.MONGO_URI, {
+// //     useNewUrlParser: true,
+// //     useUnifiedTopology: true,
+// //   })
+// //   .then(() => console.log("MongoDB Connected"))
+// //   .catch((err) => console.error("MongoDB Connection Error:", err));
 
-app.listen(5080, () => {
-  console.log("Server started at http://localhost:5080");
-});
+// // app.listen(5080, () => {
+// //   console.log("Server started at http://localhost:5080");
+// // });
