9323e00a429483428a13dc72cb7d9ca1eac8f7ee
JasonPerera30 <jason.manthila@gmail.com>
2025-05-22 21:50:20 +0530

    Merging route file

--- Backend/routes/branchRoutes.js
+++ Backend/routes/branchRoutes.js
@@ -1,12 +1,28 @@
-import express from 'express';
+import mongoose from "mongoose";
+import express from "express";
+import Branch from "../models/Branch.js";
+import { createBranch, deleteBranch, getBranch, updateBranch } from "../controllers/branch.controller.js";
 import { getBranchById } from '../controllers/branchController.js';
-import { getBranches , getBranchByOrderId} from '../controllers/branchController.js'; // Import the controller functions
-
+import { getBranches , getBranchByOrderId} from '../controllers/branchController.js';
+ 
 const router = express.Router();
-
-// Route to get branch details by branchId
+ 
+// Get branch details
+router.get("/", getBranch);
+ 
+//Create branch
+router.post("/", createBranch);
+ 
+//delete branch
+router.delete("/:id", deleteBranch);
+ 
+//update branch
+router.put("/:id", updateBranch);
+ 
 router.get('/:branchId', getBranchById);
+ 
 router.get('/', getBranches);
-router.get('/by-order/:orderId', getBranchByOrderId); // Route to get branch by orderId
-
+ 
+router.get('/by-order/:orderId', getBranchByOrderId);
+ 
 export default router;
